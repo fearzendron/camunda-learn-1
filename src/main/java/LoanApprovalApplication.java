51d966b6d7f97e537094c08b8abbcfc6d51e540e
Fernando Zendron <fearzendron@gmail.com>
2015-01-03 00:36:15 -0200

    First commit

---
+++ src/main/java/LoanApprovalApplication.java
@@ -0,0 +1,7 @@
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Loan Approval App")
+public class LoanApprovalApplication extends ServletProcessApplication {
+
+}
